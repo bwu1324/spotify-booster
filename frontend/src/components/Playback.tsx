a6c70d3db98d94663d708908ef282d290bc04626
Alexander Eilert <57025593+alexjeilert@users.noreply.github.com>
2023-03-29 01:32:30 -0500

    Added Media Buttons

--- frontend/src/components/Playback.tsx
+++ frontend/src/components/Playback.tsx
@@ -4,6 +4,39 @@ import LinearProgress, {
 } from '@mui/material/LinearProgress';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Container from '@mui/material/Container/Container';
+import IconButton from '@mui/material/IconButton';
+import { PlayArrow, FastForward, FastRewind, Pause } from '@mui/icons-material';
+import { useState } from 'react';
+
+function PlaybackBar() {
+  <script src="https://sdk.scdn.co/spotify-player.js"></script>;
+  const progress = 10;
+  const total = 20;
+
+  const [clicked, setClicked] = useState<boolean>(false);
+  return (
+    <Container
+      maxWidth="lg"
+      style={{
+        overflowY: 'auto', // scroll on overflow
+      }}
+    >
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <IconButton>
+          <FastRewind />
+        </IconButton>
+        <IconButton onClick={() => setClicked(!clicked)}>
+          {clicked ? <PlayArrow /> : <Pause />}
+        </IconButton>
+        <IconButton>
+          <FastForward />
+        </IconButton>
+      </Box>
+      <LinearProgressWithTime current={progress} total={total} />
+    </Container>
+  );
+}
 
 function fancyTimeFormat(duration: number) {
   // Hours, minutes and seconds
@@ -24,40 +57,86 @@ function fancyTimeFormat(duration: number) {
   return ret;
 }
 
-function LinearProgressWithTime(current: number, total: number) {
-  current = 4;
-  total = 10;
+function LinearProgressWithTime(
+  props: LinearProgressProps & { current: number; total: number }
+) {
   return (
     <Box sx={{ display: 'flex', alignItems: 'center' }}>
       <Box sx={{ minWidth: 35 }}>
         <Typography variant="body2" color="text.secondary">{`${fancyTimeFormat(
-          current
+          props.current
         )}`}</Typography>
       </Box>
       <Box sx={{ width: '100%', mr: 1 }}>
-        <LinearProgress variant="determinate" value={(100 * current) / total} />
+        <LinearProgress
+          variant="determinate"
+          value={(100 * props.current) / props.total}
+        />
       </Box>
       <Box sx={{ minWidth: 35 }}>
         <Typography variant="body2" color="text.secondary">{`${fancyTimeFormat(
-          total
+          props.total
         )}`}</Typography>
       </Box>
     </Box>
   );
 }
 
-function PlaybackBar() {
-  const progress = 10;
-  const total = 20;
-  return (
-    <div
-      style={{
-        overflowY: 'auto', // scroll on overflow
-      }}
-    >
-      <LinearProgressWithTime current={progress} total={total} />
-    </div>
-  );
-}
-
 export default PlaybackBar;
+
+// import React from 'react';
+
+// import '../App.scss';
+
+// import Header from './Header';
+// import Finder from './finder/Finder';
+// import { ThemeProvider } from '@mui/material/styles';
+// import { theme } from '../theme';
+// import GlobalStyles from '@mui/material/GlobalStyles';
+
+// const Playback: React.FC = () => {
+
+// <h1>Spotify Web Playback SDK Quick Start</h1>
+//     <button id="togglePlay">Toggle Play</button>
+
+//     <script src="https://sdk.scdn.co/spotify-player.js"></script>
+//         window.onSpotifyWebPlaybackSDKReady = () => {
+//             const token = '[My access token]';
+//             const player = new Spotify.Player({
+//                 name: 'Web Playback SDK Quick Start Player',
+//                 getOAuthToken: cb => { cb(token); },
+//                 volume: 0.5
+//             });
+
+//             // Ready
+//             player.addListener('ready', ({ device_id }) => {
+//                 console.log('Ready with Device ID', device_id);
+//             });
+
+//             // Not Ready
+//             player.addListener('not_ready', ({ device_id }) => {
+//                 console.log('Device ID has gone offline', device_id);
+//             });
+
+//             player.addListener('initialization_error', ({ message }) => {
+//                 console.error(message);
+//             });
+
+//             player.addListener('authentication_error', ({ message }) => {
+//                 console.error(message);
+//             });
+
+//             player.addListener('account_error', ({ message }) => {
+//                 console.error(message);
+//             });
+
+//             document.getElementById('togglePlay').onclick = function() {
+//               player.togglePlay();
+//             };
+
+//             player.connect();
+//         }
+
+//     };
+
+//     export default Playback;
