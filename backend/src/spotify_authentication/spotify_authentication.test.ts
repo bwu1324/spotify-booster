55f9282804b1eaa376ef15124e32abdcf74007e1
Bennett Wu <57691028+bwu1324@users.noreply.github.com>
2023-03-30 10:42:30 -0500

    tests(spotify_authentication): write tests

---
+++ backend/src/spotify_authentication/spotify_authentication.test.ts
@@ -0,0 +1,173 @@
+import { assert } from 'chai';
+import sinon from 'sinon';
+
+import * as SpotifyWebApi from './spotify_api_import';
+
+import createSpotifyAuthenticator, {
+  AuthRequest,
+} from './spotify_authentication';
+import Logger from '../logger/logger';
+
+describe('Spotify Authentication Middleware', () => {
+  it('allows request with valid token', async () => {
+    sinon.stub(SpotifyWebApi, 'SpotifyAPI').callsFake(() => {
+      return {
+        access_token: '',
+        setAccessToken(token: string) {
+          this.access_token = token;
+        },
+        getMe() {
+          if (this.access_token === 'valid_token') {
+            return {
+              body: {
+                id: 'some_id',
+              },
+            };
+          }
+          throw new Error('Incorrect Token');
+        },
+      };
+    });
+
+    const spotifyAuthentication = createSpotifyAuthenticator(
+      new Logger('Test')
+    );
+
+    const req = {
+      headers: {},
+      socket: {
+        remoteAddress: 'test',
+      },
+      cookies: {
+        spotify_access_token: 'valid_token',
+      },
+    } as AuthRequest;
+    const next = sinon.spy();
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    await spotifyAuthentication(req, {} as any, next);
+
+    assert.equal(next.callCount, 1, 'Middleware calls next');
+    assert.equal(req.spotify_uid, 'some_id', 'Sets correct spotify_uid');
+  });
+
+  it('disallows request with valid token', async () => {
+    sinon.stub(SpotifyWebApi, 'SpotifyAPI').callsFake(() => {
+      return {
+        access_token: '',
+        setAccessToken(token: string) {
+          this.access_token = token;
+        },
+        getMe() {
+          if (this.access_token === 'valid_token') {
+            return {
+              body: {
+                id: 'some_id',
+              },
+            };
+          }
+          throw new Error('Incorrect Token');
+        },
+      };
+    });
+
+    const spotifyAuthentication = createSpotifyAuthenticator(
+      new Logger('Test')
+    );
+
+    const req = {
+      headers: {},
+      socket: {
+        remoteAddress: 'test',
+      },
+      cookies: {
+        spotify_access_token: 'invalid_token',
+      },
+    } as AuthRequest;
+    const next = sinon.spy();
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    await spotifyAuthentication(req, {} as any, next);
+
+    assert.equal(next.callCount, 0, 'Middleware does not call next');
+  });
+
+  it('disallows request without cookies object', async () => {
+    sinon.stub(SpotifyWebApi, 'SpotifyAPI').callsFake(() => {
+      return {
+        access_token: '',
+        setAccessToken(token: string) {
+          this.access_token = token;
+        },
+        getMe() {
+          if (this.access_token === 'valid_token') {
+            return {
+              body: {
+                id: 'some_id',
+              },
+            };
+          }
+          throw new Error('Incorrect Token');
+        },
+      };
+    });
+
+    const spotifyAuthentication = createSpotifyAuthenticator(
+      new Logger('Test')
+    );
+
+    const req = {
+      headers: {},
+      socket: {
+        remoteAddress: 'test',
+      },
+    } as AuthRequest;
+    const next = sinon.spy();
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    await spotifyAuthentication(req, {} as any, next);
+
+    assert.equal(next.callCount, 0, 'Middleware does not call next');
+  });
+
+  it('disallows request with empty cookies', async () => {
+    sinon.stub(SpotifyWebApi, 'SpotifyAPI').callsFake(() => {
+      return {
+        access_token: '',
+        setAccessToken(token: string) {
+          this.access_token = token;
+        },
+        getMe() {
+          if (this.access_token === 'valid_token') {
+            return {
+              body: {
+                id: 'some_id',
+              },
+            };
+          }
+          throw new Error('Incorrect Token');
+        },
+      };
+    });
+
+    const spotifyAuthentication = createSpotifyAuthenticator(
+      new Logger('Test')
+    );
+
+    const req = {
+      headers: {},
+      socket: {
+        remoteAddress: 'test',
+      },
+      cookies: {
+        spotify_access_token: '',
+      },
+    } as AuthRequest;
+    const next = sinon.spy();
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    await spotifyAuthentication(req, {} as any, next);
+
+    assert.equal(next.callCount, 0, 'Middleware does not call next');
+  });
+});
