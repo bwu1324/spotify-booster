ef90b2a59b48356ee7b0bc18bd23a1070cfa56a4
Bennett Wu <57691028+bwu1324@users.noreply.github.com>
2023-03-31 15:03:09 -0500

    fix(cors): configure cors without wildcard

--- backend/src/webserver/cors_import.ts
+++ backend/src/webserver/cors_import.ts
@@ -1,4 +1,7 @@
 import cors from 'cors';
 
 // Wrapper import for cors to avoid stubbing bug
-export const CORS = cors();
+export const CORS = cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+});
